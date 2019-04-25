580899c73d09a6874318b6a784c1e3f4ded7e7a9
danieloi <mayowadan17@gmail.com>
2019-04-25 12:31:25 +0100

    pulled data, YEAH!!!

--- index.js
+++ index.js
@@ -1,24 +1,66 @@
 const Nightmare = require("nightmare");
+const db = require("./db");
 
-async function getAffirmation() {
-  let nightmare = new Nightmare({ show: true });
-  //   let nightmare = new Nightmare();
+// async function pullAllLinksonPage(page) {
+//   let nightmare = new Nightmare({ show: true });
+
+//   try {
+//     const result = await nightmare
+//       .goto(
+//         page == 1
+//           ? `https://www.affirmation-train.org/category/affirmation`
+//           : `https://www.affirmation-train.org/category/affirmation/page/${page}/`
+//       )
+//       .wait(".more-link")
+//       .evaluate(() => {
+//         const hrefs = [...document.querySelectorAll(".more-link")].map(
+//           element => element.href
+//         );
+
+//         // const results = Promise.all(promises);
+//         return hrefs;
+//       })
+//       .end();
+
+//     return result;
+//   } catch (e) {
+//     console.error(e);
+//     return undefined;
+//   }
+// }
+
+// function gatherLinks() {
+//   pullAllLinksonPage(i).then(results => {
+//     links.push(...results);
+
+//     db.get("links")
+//       .push(...results)
+//       .write();
+//     console.log(links);
+//     i++;
+//     if (i < 76) {
+//       gatherLinks();
+//     }
+//   });
+// }
+
+// gatherLinks();
+
+async function pullContentFromPage(page) {
+  let nightmare = new Nightmare();
 
   try {
     const result = await nightmare
-      .goto("https://www.affirmation-train.org/category/affirmation")
-      .wait(".more-link")
-      .wait(2000)
-      .click(".more-link")
+      .goto(page)
       .wait(".entry")
-      .wait(3000)
       .evaluate(() => {
         return [...document.querySelector(".entry").children].map(element => {
           if (
             element.constructor.name == "HTMLParagraphElement" &&
             element.innerText
-          )
+          ) {
             return element.innerText;
+          }
         });
       })
       .end();
@@ -30,6 +72,38 @@ async function getAffirmation() {
   }
 }
 
-getAffirmation().then(results => {
-  console.log(results);
-});
+let i = 731;
+links = db.get("links").value();
+
+function fetchPosts() {
+  pullContentFromPage(links[i]).then(content => {
+    db.get("posts")
+      .push({ link: links[i], content })
+      .write();
+    i++;
+    if (i < links.length) {
+      fetchPosts();
+    }
+  });
+}
+
+fetchPosts();
+
+// {
+//   return await nightmare
+//     .goto(element.href)
+//     .wait(".entry")
+//     .wait(2000)
+//     .evaluate(() => {
+//       return [...document.querySelector(".entry").children].map(
+//         element => {
+//           if (
+//             element.constructor.name == "HTMLParagraphElement" &&
+//             element.innerText
+//           ) {
+//             return element.innerText;
+//           }
+//         }
+//       );
+//     });
+// }
